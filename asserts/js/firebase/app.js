039deed2dfa5f26baab62a8fda35920fcfe4cf71
eduardosousaa <eduardosousa1718@gmail.com>
2023-04-27 12:47:46 -0300

    Projeto quase finalizado

---
+++ asserts/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4XflFHvjHWcBpm6qtDj5JReQ_FlRrdho",
+  authDomain: "projetomundoinvertido-3de12.firebaseapp.com",
+  projectId: "projetomundoinvertido-3de12",
+  storageBucket: "projetomundoinvertido-3de12.appspot.com",
+  messagingSenderId: "1023913587927",
+  appId: "1:1023913587927:web:363b37bf6162646946ec92"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
